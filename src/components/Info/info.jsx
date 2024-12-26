d139b089eca627a21177da2b1ffcfecd9da623a1
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-12-26 12:25:49 -0400

    tailwind configuration

---
+++ src/components/Info/info.jsx
@@ -0,0 +1,10 @@
+function Info() {
+  return (
+    <div class="header__info">
+      <p class="header__info__number">+1 (929) 377-0976</p>
+      <p class="header__info__open">Open 7 days 8:00 am - 8:00 pm</p>
+    </div>
+  );
+}
+
+export default Info;
