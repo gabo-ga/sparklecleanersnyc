257520e02ef0722328b6163cba564b9e643fa1ea
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-12-27 11:27:52 -0400

    commit random

--- src/components/Info/info.jsx
+++ src/components/Info/info.jsx
@@ -1,8 +1,12 @@
 function Info() {
   return (
-    <div class="header__info">
-      <p class="header__info__number">+1 (929) 377-0976</p>
-      <p class="header__info__open">Open 7 days 8:00 am - 8:00 pm</p>
+    <div class="flex p-2 items-start gap-4 self-stretch">
+      <p class="text-black text-xs font-normal font-['Inter']">
+        +1 (929) 377-0976
+      </p>
+      <p class="text-black text-xs font-normal font-['Inter']">
+        Open 7 days 8:00 am - 8:00 pm
+      </p>
     </div>
   );
 }
