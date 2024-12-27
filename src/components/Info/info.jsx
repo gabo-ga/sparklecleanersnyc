b766177d9beef5f9d731a33419cf1181661e7fea
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-12-27 11:31:57 -0400

    info component

--- src/components/Info/info.jsx
+++ src/components/Info/info.jsx
@@ -1,10 +1,8 @@
 function Info() {
   return (
     <div class="flex p-2 items-start gap-4 self-stretch">
-      <p class="text-black text-xs font-normal font-['Inter']">
-        +1 (929) 377-0976
-      </p>
-      <p class="text-black text-xs font-normal font-['Inter']">
+      <p class="text-black text-xs font-normal">+1 (929) 377-0976</p>
+      <p class="text-black text-xs font-normal">
         Open 7 days 8:00 am - 8:00 pm
       </p>
     </div>
