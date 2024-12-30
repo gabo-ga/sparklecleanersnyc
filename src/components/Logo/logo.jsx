ca4eb2c2b19578425fe9fbc07144260b00ddea26
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-12-30 10:17:09 -0400

    logo svg file src change

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -2,7 +2,11 @@ function Logo() {
   return (
     <div class="flex items-start">
       <div class="flex items-center gap-1 py-0 px-2.5 h-9" id="logo">
-        <img src="../src/assets/Logo.svg" alt="logo" class="h-10 w-7" />
+        <img
+          src="https://srv1562-files.hstgr.io/6d156cfa01a14dd2/files/public_html/assets/Logo.svg"
+          alt="logo"
+          class="h-10 w-7"
+        />
         <h1 class="text-white text-base font font-medium">
           Spark Cleaners nyc
         </h1>
