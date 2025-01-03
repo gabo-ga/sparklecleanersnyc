46b149354cc353fed03426711f2b1543681803df
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-01-03 12:46:45 -0400

    fix:footer ready

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,16 +1,15 @@
 function Footer() {
   return (
     <footer>
-      <div class="px-11 py-3 flex flex-col items-start bg-sky-900">
-        <div class="flex flex-row gap-2 text-white">
+      <div class="px-11 py-3 flex flex-col items-start bg-sky-900 w-screen">
+        <div class="flex flex-row gap-2 text-white text-md">
           <img src="./images/Logo.svg" alt="logo" />
-          <h2>Sparkle Cleaners nyc</h2>
+          <h2 class="text-md">Sparkle Cleaners nyc</h2>
         </div>
-        <h3 class="text-white text-md font-normal">Sparkle Cleaners nyc</h3>
-        <h3 class="footer-info__address">
-          50-28 61st Street Woodside, NY 11377
-        </h3>
-        <p class="footer-info__number">(929) 377-0976</p>
+        {/*<h3 class="text-white text-md font-normal">Sparkle Cleaners nyc</h3>*/}
+        <h3 class="text-slate-300 text-xs">50-28 61st Street</h3>
+        <h3 class="text-slate-300 text-xs">Woodside, NY 11377</h3>
+        <p class="text-slate-400 text-xs">(929) 377-0976</p>
       </div>
     </footer>
   );
