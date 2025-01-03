a412bfd3fd098cadd5a3368bbd419e0e78249136
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-01-03 16:13:46 -0400

    logo not working

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -3,7 +3,7 @@ function Logo() {
     <div class="flex items-start">
       <div class="flex items-center gap-1 py-0 px-2.5 h-9" id="logo">
         <img
-          src="https://srv1562-files.hstgr.io/6d156cfa01a14dd2/files/public_html/assets/Logo.svg"
+          src="/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/Logo.svg"
           alt="logo"
           class="h-10 w-7"
         />
