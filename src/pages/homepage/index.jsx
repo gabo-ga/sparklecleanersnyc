c8ee01824eb44e82957504db6c92ecf8ec04714f
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-01-03 06:18:00 -0400

    hero section

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,7 +1,13 @@
 import Header from "@/layout/Header/header";
+import Hero from "@/layout/Hero/hero";
 
 function HomePage() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+    </>
+  );
 }
 
 export default HomePage;
