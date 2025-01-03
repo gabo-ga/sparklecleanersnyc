0d7651ce47a3bbd27f02dfa4b9d3a98baa76b618
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-01-03 08:31:59 -0400

    feat:carousel

--- src/layout/Carousel/index.jsx
+++ src/layout/Carousel/index.jsx
@@ -5,15 +5,44 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function CarouselSection() {
   return (
     <>
-      <Carousel>
+      <Carousel className="py-8 px-9">
         <CarouselContent>
-          <CarouselItem className="basis-1/3"></CarouselItem>
-          <CarouselItem></CarouselItem>
-          <CarouselItem></CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Card>
+              <CardContent className="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/background.jpg')] bg-cover h-36"></CardContent>
+              <CardFooter>Houses</CardFooter>
+            </Card>
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Card>
+              <CardContent className="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/background.jpg')] bg-cover h-36"></CardContent>
+              <CardFooter>Apartments</CardFooter>
+            </Card>
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Card>
+              <CardContent className="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/background.jpg')] bg-cover h-36"></CardContent>
+              <CardFooter>Offices</CardFooter>
+            </Card>
+          </CarouselItem>
+          <CarouselItem className="basis-1/3">
+            <Card>
+              <CardContent className="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/background.jpg')] bg-cover h-36"></CardContent>
+              <CardFooter>Postconstruction</CardFooter>
+            </Card>
+          </CarouselItem>
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
