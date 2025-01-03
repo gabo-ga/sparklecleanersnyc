24db5d33a15c88ebcfa649cb90f6b1a6f2cb857c
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-01-03 11:02:04 -0400

    fix:info size of tezt fox

--- src/layout/Contact/index.jsx
+++ src/layout/Contact/index.jsx
@@ -2,27 +2,27 @@ function Contact() {
   return (
     <article>
       <div class="flex flex-col items-center justify-center gap-5 px-9 py-5">
-        <h2 class="text-black text-3xl font-medium">Amazing benefits!</h2>
-        <p class="text-justify items-center text-lg">
+        <h2 class="text-black text-xl font-medium">Amazing benefits!</h2>
+        <p class="text-justify items-center text-sm">
           Choose us for exceptional cleaning tailores to New Yorkes. Our trained
           and professional team ensure a spotless, safe home. Enjoy flexible
           scheduling, customized plans, same-day cleaning and reliable services.
           Trust us to transform your space.
         </p>
-        <div class="flex flex-row items-start gap-5 text-2xl font-semibold text-[#aae858]">
+        <div class="flex flex-row items-start gap-5 text-xl font-semibold text-[#aae858]">
           <h3>Send us a message!</h3>
           <img
             src="./images/whatsapp_logo.png"
             alt="whatsapp logo"
-            width="50"
-            height="50"
+            width="30"
+            height="30"
           />
 
           <img
             src="./images/messages_logo.jpeg"
             alt="messages logo"
-            width="50"
-            height="50"
+            width="30"
+            height="30"
           />
         </div>
       </div>
