de6ebc30e14574eab4dad19d35e92f706dddb0f9
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-01-06 19:56:37 -0400

    review responsive for tablet

--- src/layout/Contact/index.jsx
+++ src/layout/Contact/index.jsx
@@ -8,30 +8,33 @@ function Contact() {
   };
 
   return (
-    <article>
-      <div class="flex flex-col items-center justify-center gap-5 px-9 py-5">
-        <h2 class="text-black text-xl font-medium font-Poppins">
-          Amazing benefits!
-        </h2>
-        <p class="text-justify items-center text-sm font-Poppins">
-          Choose us for exceptional cleaning tailores to New Yorkes. Our trained
-          and professional team ensure a spotless, safe home. Enjoy flexible
-          scheduling, customized plans, same-day cleaning and reliable services.
-          Trust us to transform your space.
-        </p>
-        <div class="flex flex-row items-start gap-3 text-xl font-semibold text-[#aae858] font-Poppins">
-          <h3>Send us a message!</h3>
-          <div
-            onClick={handleWhatsappClick}
-            class="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/whatsapp_logo.png')] bg-cover bg-no-repeat size-7"
-          ></div>
-          <div
-            onClick={handleImessageClick}
-            class="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/messages_logo.jpeg')] bg-cover bg-no-repeat size-7"
-          ></div>
+    <div class="flex flex-row w-full items-center md:justify-center gap-5">
+      <article>
+        <div class="flex flex-col items-center justify-center gap-5 px-9 py-5 max-w-md">
+          <h2 class="text-black text-xl font-medium font-Poppins">
+            Amazing benefits!
+          </h2>
+          <p class="text-justify items-center text-sm font-Poppins text-wrap">
+            Choose us for exceptional cleaning tailores to New Yorkes. Our
+            trained and professional team ensure a spotless, safe home. Enjoy
+            flexible scheduling, customized plans, same-day cleaning and
+            reliable services. Trust us to transform your space.
+          </p>
+          <div class="flex flex-row items-start gap-3 text-xl font-semibold text-[#aae858] font-Poppins">
+            <h3>Send us a message!</h3>
+            <div
+              onClick={handleWhatsappClick}
+              class="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/whatsapp_logo.png')] bg-cover bg-no-repeat size-7"
+            ></div>
+            <div
+              onClick={handleImessageClick}
+              class="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/messages_logo.jpeg')] bg-cover bg-no-repeat size-7"
+            ></div>
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
+      <div class="bg-[url('/Users/gabrielgonzales/Documents/sparkle-cleaners/sparklecleanersnyc/src/assets/man.jpg')] bg-cover bg-no-repeat h-72 w-64 hidden md:block"></div>
+    </div>
   );
 }
 
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -11,7 +11,7 @@ function HomePage() {
       <Hero></Hero>
       <CarouselSection></CarouselSection>
       <Contact></Contact>
-      <Footer></Footer>
+      {/*<Footer></Footer>*/}
     </>
   );
 }
