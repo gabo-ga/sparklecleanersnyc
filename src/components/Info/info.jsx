b09a58eb3d7138f776db87aa26a0a8d9e2b94a90
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-02-07 17:18:39 -0400

    feat: agrega ícono de teléfono y mejora el formato del número en el componente Info

--- src/components/Info/info.jsx
+++ src/components/Info/info.jsx
@@ -1,8 +1,15 @@
 function Info() {
   return (
     <div class="flex p-1 gap-3">
-      <p class="text-black text-xs font-normal">+1 (929) 377-0976</p>
-      <p class="text-black text-xs font-normal">
+      <div class="flex gap-1">
+        <img
+          src="../../../src/assets/phone.svg"
+          alt="icon"
+          class="w-3 md:w-4"
+        ></img>
+        <p class="text-black text-xs font-normal md:text-sm">(929) 377-0976</p>
+      </div>
+      <p class="text-black text-xs font-normal md:text-sm">
         Open 7 days 8:00 am - 8:00 pm
       </p>
     </div>
